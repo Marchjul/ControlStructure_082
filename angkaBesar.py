a8db0b9758a31191c50c65322e70b00cdea93b4a
Marchjul <marchellogiovani28@gmail.com>
2026-09-22 11:10:39 +0700

    Angka terbesar update jika ada angka yang sama maka tidak ada angka terbesar

--- angkaBesar.py
+++ angkaBesar.py
@@ -2,10 +2,14 @@ angka1 = int(input("Masukan Angka Pertama : "))
 angka2 = int(input("Masukan Angka Kedua : "))
 angka3 = int(input("Masukan Angka Ketiga : "))
 
-if angka1 >= angka2 and angka1 >= angka3:
+if angka1 > angka2 and angka1 > angka3:
     terbesar = angka1
-elif angka2 >= angka1 and angka2 >= angka3:
+    print("Angka Terbesar adalah : ", terbesar)
+elif angka2 > angka1 and angka2 > angka3:
     terbesar = angka2
-else:
+    print("Angka Terbesar adalah : ", terbesar)
+elif angka3 > angka1 and angka3 > angka2:
     terbesar = angka3
-print(f"Angka Terbesar Adalah : {terbesar}")
+    print("Angka Terbesar adalah : ", terbesar)
+else:
+    print("Tidak ada angka terbesar")
