ef63ffb2e8249af36621f9ee58e635696fe04526
Marchjul <marchellogiovani28@gmail.com>
2026-09-22 10:28:45 +0700

    Evaluasi Nilai

---
+++ nilai.py
@@ -0,0 +1,11 @@
+nilai = int(input("Nilai yang diperoleh : "))
+if nilai >= 90:
+    print("Excellent performance")
+elif nilai >= 80:
+    print("Very Good performance")
+elif nilai >= 70:
+    print("Good performance")
+elif nilai >= 60:
+    print("Average performance")
+else :
+    print("BELAJAR LAGI!!!")
